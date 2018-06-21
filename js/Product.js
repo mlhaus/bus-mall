4d04f2f0dc078d48b41ff71e0d9350a491a57c91
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-06-21 13:21:07 -0500

    Set up displayProducts() to display random images

--- js/Product.js
+++ js/Product.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function Product(name, imgSrc, description, price) {
+function Product(name, filePath, description, price) {
   this.name = name || 'No Name';
-  this.imgSrc = imgSrc || 'image-not-available.jpg';
+  this.filePath = filePath || 'image-not-available.jpg';
   this.description = description || '';
   this.price = price || 0.00;
   this.showCount = 0;
@@ -13,32 +13,71 @@ Product.all = [];
 Product.totalVoteCount = 0;
 
 function initialize() {
-  new Product("R2D2 Suitcase", "bag.jpg");
-  new Product("Banana Slicer", "banana.jpg");
-  new Product("iPad/Toilet Paper Stand", "bathroom.jpg");
-  new Product("Toeless Rain Boots", "boots.jpg");
-  new Product("Toaster/Hot Plate/Coffee Maker", "breakfast.jpg");
-  new Product("Meatball Bubble Gum", "bubblegum.jpg");
-  new Product("Oddly Designed Chair", "chair.jpg");
-  new Product("Legends of Cthulhu Action Figure", "cthulhu.jpg");
-  new Product("Duck Beak for Dogs", "dog-duck.jpg");
-  new Product("Canned Dragon Meat", "dragon.jpg");
-  new Product("Plastic Silverware Pen Caps", "pen.jpg");
-  new Product("Pet Sweep Dust Boots", "pet-sweep.jpg");
-  new Product("Pizza Slicer Scissors", "scissors.jpg");
-  new Product("Shark Sleeping Bag", "shark.jpg");
-  new Product("Baby Onesie Floor Sweeper", "sweep.png");
-  new Product("Star Wars Tauntaun Sleeping Bag", "tauntaun.jpg");
-  new Product("Canned Unicorn Meat", "unicorn.jpg");
-  new Product("Moving Tentacle USB Drive", "usb.gif");
-  new Product("Poorly Designed Water Can", "water-can.jpg");
-  new Product("Interestingly Designed Wine Glass", "wine-glass.jpg");
+  new Product('R2D2 Suitcase', 'img/bag.jpg');
+  new Product('Banana Slicer', 'img/banana.jpg');
+  new Product('iPad/Toilet Paper Stand', 'img/bathroom.jpg');
+  new Product('Toeless Rain Boots', 'img/boots.jpg');
+  new Product('Toaster/Hot Plate/Coffee Maker', 'img/breakfast.jpg');
+  new Product('Meatball Bubble Gum', 'img/bubblegum.jpg');
+  new Product('Oddly Designed Chair', 'img/chair.jpg');
+  new Product('Legends of Cthulhu Action Figure', 'img/cthulhu.jpg');
+  new Product('Duck Beak for Dogs', 'img/dog-duck.jpg');
+  new Product('Canned Dragon Meat', 'img/dragon.jpg');
+  new Product('Plastic Silverware Pen Caps', 'img/pen.jpg');
+  new Product('Pet Sweep Dust Boots', 'img/pet-sweep.jpg');
+  new Product('Pizza Slicer Scissors', 'img/scissors.jpg');
+  new Product('Shark Sleeping Bag', 'img/shark.jpg');
+  new Product('Baby Onesie Floor Sweeper', 'img/sweep.png');
+  new Product('Star Wars Tauntaun Sleeping Bag', 'img/tauntaun.jpg');
+  new Product('Canned Unicorn Meat', 'img/unicorn.jpg');
+  new Product('Moving Tentacle USB Drive', 'img/usb.gif');
+  new Product('Poorly Designed Water Can', 'img/water-can.jpg');
+  new Product('Interestingly Designed Wine Glass', 'img/wine-glass.jpg');
 
   console.log('Initialized Products: ', Product.all);
+}
+
+let numProductsToDisplay = 3;
+let productsPreviouslyDisplayed = [];
+let productsToDisplay = [];
+function displayProducts() {
+  productsPreviouslyDisplayed = productsToDisplay;
+  productsToDisplay = [];
+  if(Product.all.length > numProductsToDisplay * 2) {
+    while(productsToDisplay.length < numProductsToDisplay){
+      let randomNumber = Math.floor(Math.random() * Product.all.length);
+      if(productsToDisplay.includes(randomNumber) || productsPreviouslyDisplayed.includes(randomNumber)){
+        continue;
+      }
+      productsToDisplay.push(randomNumber);
+    }
+    console.log(productsToDisplay);
+    let imageRow = document.getElementById('productImages');
+    console.log(imageRow);
+    imageRow.innerHTML = '';
+    for(let i = 0; i < numProductsToDisplay; i++){
+      let div = document.createElement('div');
+      div.classList.add('column');
+      div.style.width = ((100 / numProductsToDisplay) - ((numProductsToDisplay - 1) / numProductsToDisplay)) + '%';
+      imageRow.appendChild(div);
+      let h4 = document.createElement('h4');
+      h4.textContent = Product.all[productsToDisplay[i]].name;
+      div.appendChild(h4);
+      let img = document.createElement('img');
+      img.setAttribute('src', Product.all[productsToDisplay[i]].filePath);
+      img.setAttribute('alt', Product.all[productsToDisplay[i]].description);
+      img.setAttribute('title', Product.all[productsToDisplay[i]].description);
+      div.appendChild(img);
+    }
+  }
+  else {
+    console.log('You need to add at least ' + (numProductsToDisplay * 2 + 1 - Product.all.length) + ' more product(s).');
+  }
 }
 
 window.addEventListener('load', function() {
   if (Product.all.length === 0) {
     initialize();
   }
+  displayProducts();
 });
